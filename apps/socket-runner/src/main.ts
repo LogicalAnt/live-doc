24a32a6cb5f4ef28b6118e08e45936a2a1dd79f7
Shakil Ahmed <shakil147258@gmail.com>
2020-04-13 19:39:52 +0600

    feat: integrate socket.io

--- apps/socket-runner/src/main.ts
+++ apps/socket-runner/src/main.ts
@@ -7,12 +7,20 @@ import * as express from 'express';
 
 const app = express();
 
-app.get('/api', (req, res) => {
+const http = require('http').createServer(app);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
   res.send({ message: 'Welcome to socket-runner!' });
 });
 
 const port = process.env.port || 3333;
+
+io.on('connection', function(socket) {
+  console.log('a user connected', socket);
+});
+
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
