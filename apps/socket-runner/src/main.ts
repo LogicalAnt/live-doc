07c6e192ff2cf6b1cd10dacc0537777d566b44d0
Shakil Ahmed <shakil147258@gmail.com>
2020-04-16 14:57:00 +0600

    feat: emit editor size

--- apps/socket-runner/src/main.ts
+++ apps/socket-runner/src/main.ts
@@ -1,26 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  **/
 
 import * as express from 'express';
-
 const app = express();
-
 const http = require('http').createServer(app);
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const io = require('socket.io')(http);
 app.get('/', (req, res) => {
-  res.send({ message: 'Welcome to socket-runner!' });
+  res.send('Welcome to socket-runner!');
 });
 
-const port = process.env.port || 3333;
+const port = process.env.SOCKET_SERVER_PORT || 3333;
 
 io.on('connection', function(socket) {
-  console.log('a user connected', socket);
+  console.log('a user connected');
+
+  socket.on('editorText', value => {
+    io.emit('emittedText', value);
+  });
+
+  socket.on('editorSize', value => {
+    io.emit('emittedEditorSize', value);
+  });
 });
 
-const server = app.listen(port, () => {
+const server = http.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
