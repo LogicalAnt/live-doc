349d3e1e55f237cd7306581e32934e13f79da342
Shakil Ahmed <shakil147258@gmail.com>
2020-04-18 19:58:44 +0600

    feat: add router for group editing

--- apps/live-doc/src/main.tsx
+++ apps/live-doc/src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './app/app';
+import { Home } from './app/Components/Home';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/group/:id">
+        <Home />
+      </Route>
+      <Route path="*">
+        <p>page not found</p>
+      </Route>
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
