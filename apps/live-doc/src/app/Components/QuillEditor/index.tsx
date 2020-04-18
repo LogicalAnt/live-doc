a847d0c14ea4cdc9e800d0eda21d257194b4129a
Shakil Ahmed <shakil147258@gmail.com>
2020-04-18 20:01:09 +0600

    feat: WIP group editing

--- apps/live-doc/src/app/Components/QuillEditor/index.tsx
+++ apps/live-doc/src/app/Components/QuillEditor/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 
 interface EmitData {
@@ -10,23 +11,41 @@ interface EmitData {
 export const QuillEditor = () => {
   const [value, setValue] = useState('');
   const socket = io('http://localhost:3333');
-  socket.on('emittedText', (value: string) => {
-    setValue(value);
+  const { id } = useParams();
+
+  //group data handler
+  useEffect(() => {
+    fetch(`http://localhost:3333/group/${id}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setValue(data.data);
+      });
+  }, [id]);
+
+  // receive emitted data from socket server
+  socket.on('emittedText', ({ content }) => {
+    setValue(content);
   });
 
-  const handleChange = content => {
-    socket.emit('editorText', content);
+  // emit editor data
+  const emitEditorData = (content: any) => {
+    socket.emit('editorText', { content: content, id: id });
   };
 
   return (
-    <ReactQuill
-      style={{ height: 300 }}
-      placeholder="Type and share..."
-      theme="snow"
-      value={value}
-      onChange={(content, delta) => {
-        handleChange(content);
-      }}
-    />
+    <>
+      <ReactQuill
+        style={{ height: 300 }}
+        placeholder="Type and share..."
+        theme="snow"
+        value={value}
+        onChange={(content, delta, editor) => {
+          emitEditorData(content);
+        }}
+      />
+    </>
   );
 };
--- apps/socket-runner/src/main.ts
+++ apps/socket-runner/src/main.ts
@@ -5,20 +5,40 @@
  **/
 
 import * as express from 'express';
+const cors = require('cors');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const port = process.env.SOCKET_SERVER_PORT || 3333;
+
+//save editor test against id
+const editorText = [];
+
+//fix up cors problem
+app.use(cors());
+
+//home route
 app.get('/', (req, res) => {
   res.send('Welcome to socket-runner!');
 });
 
-const port = process.env.SOCKET_SERVER_PORT || 3333;
+//group route
+app.get('/group/:id', (req, res) => {
+  const { id } = req.params;
+  if (!editorText[id]) editorText[id] = { data: '' };
+  //return { data: editorText[id] };
+  console.log({ data: editorText[id] });
+  res.send({ data: editorText[id] });
+});
 
+// handle socket connections and emits
 io.on('connection', function(socket) {
   console.log('a user connected');
 
-  socket.on('editorText', value => {
-    io.emit('emittedText', value);
+  socket.on('editorText', ({ content, id }) => {
+    //id for group data store
+    editorText[id] = content;
+    io.emit('emittedText', { content });
   });
 
   socket.on('editorSize', value => {
@@ -26,6 +46,7 @@ io.on('connection', function(socket) {
   });
 });
 
+// launch server
 const server = http.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
